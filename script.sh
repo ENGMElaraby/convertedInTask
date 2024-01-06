25e66737e5be3717b0d230d798cfbb2868d58ff8
Mohammed Elaraby <35893057+ENGMElaraby@users.noreply.github.com>
2024-01-06 19:01:59 +0200

    sh script.sh

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 composer install
 php artisan sail:install
 ./vendor/bin/sail up
+cp .env.example .env
 
-
-docker-compose up -d --build --force-recreate
-docker-compose run --rm composer install
-docker-compose run --rm artisan config:clear
-docker-compose run --rm artisan config:cache
-docker-compose run --rm artisan optimize:clear
-docker-compose run --rm artisan migrate
-docker-compose run --rm artisan db:seed
+#docker-compose up -d --build --force-recreate
+#docker-compose run --rm composer install
+#docker-compose run --rm artisan config:clear
+#docker-compose run --rm artisan config:cache
+#docker-compose run --rm artisan optimize:clear
+#docker-compose run --rm artisan migrate
+#docker-compose run --rm artisan db:seed
 open http://localhost:8800
